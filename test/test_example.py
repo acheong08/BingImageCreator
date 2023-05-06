f5b1bad54e346ec833e0a15229e70fb3a1474522
Youth <qcgm197874@gmail.com>
2023-05-06 12:26:59 +0800

    add optional file_name parameter to save_images method and launch configuration for running pytest (#27)

---
+++ test/test_example.py
@@ -0,0 +1,16 @@
+import os,shutil
+from src.BingImageCreator import ImageGen
+def test_save_images():
+    # create a temporary output directory for testing purposes
+    test_output_dir = "test_output"
+    os.mkdir(test_output_dir)
+    # download a test image
+    test_image_url = "https://picsum.photos/200"
+    gen = ImageGen(auth_cookie='')
+    gen.save_images([test_image_url], test_output_dir, )
+    gen.save_images([test_image_url], test_output_dir, file_name="test_image")
+    # check if the image was downloaded and saved correctly
+    assert os.path.exists(os.path.join(test_output_dir, "test_image_0.jpeg"))
+    assert os.path.exists(os.path.join(test_output_dir, "0.jpeg"))
+    # remove the temporary output directory
+    shutil.rmtree(test_output_dir)
